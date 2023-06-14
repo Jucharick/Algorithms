1bac7f0c517076a5bd944f04280b4079c75035c0
Julia Ch <juliecha@yandex.ru>
2023-06-14 18:14:51 +0300

    Add Sem_1

---
+++ Sem_1.java
@@ -0,0 +1,125 @@
+import java.sql.Date;
+import java.util.Scanner;
+
+
+public class Sem_1 {
+    public static void main(String[] args) {
+        task4();
+    }
+// РќРµРѕР±С…РѕРґРёРјРѕ РЅР°РїРёСЃР°С‚СЊ Р°Р»РіРѕСЂРёС‚Рј, СЃС‡РёС‚Р°СЋС‰РёР№ СЃСѓРјРјСѓ РІСЃРµС… С‡РёСЃРµР» РѕС‚ 1 РґРѕ N. 
+    public static void task1(){ // O(n/2) == O(n) == O(2n)
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ N: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum+= i;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+
+    // РќРµРѕР±С…РѕРґРёРјРѕ РІС‹РІРµСЃС‚Рё РІСЃРµ РїСЂРѕСЃС‚С‹Рµ С‡РёСЃР»Р° РѕС‚ 2 Р”Рѕ N.
+    public static void task2(){ //O(n^2) + O(n) = O(n^2)
+        int n = 1000;
+        boolean flag;
+        for (int i = 2; i <= n; i++) {
+            flag = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0){
+                    flag = false;
+                }
+            }
+            if (flag){
+                System.out.print(i + " ");
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            //..
+        }
+    }
+
+
+    // РќРµРѕР±С…РѕРґРёРјРѕ РЅР°РїРёСЃР°С‚СЊ Р°Р»РіРѕСЂРёС‚Рј РїРѕРёСЃРєР° РІСЃРµС… РґРѕСЃС‚СѓРїРЅС‹С… РєРѕРјР±РёРЅР°С†РёР№ 
+    // (РїРѕСЃС‡РёС‚Р°С‚СЊ РєРѕР»РёС‡РµСЃС‚РІРѕ) 
+    // РґР»СЏ РєРѕР»РёС‡РµСЃС‚РІР° РєСѓР±РёРєРѕРІ K СЃ РєРѕР»РёС‡РµСЃС‚РІРѕРј РіСЂР°РЅРµР№ N
+    // 1 2 3 4 5 6
+    // 4 3 2 1 1 1
+
+    public static void task3(){
+        int count = 4;
+        int faces = 200;
+        // Date startData = new Date(System.currentTimeMillis());
+        System.out.println(recursiveCount(1, count, faces));
+        // Date endData = new Date(System.currentTimeMillis());
+        // Long chislo = startData.getTime() - endData.getTime();
+        // System.out.println(chislo);
+    }
+
+    public static int recursiveCount(int countNow, int maxCount, int faces){ // O(k^n)
+        int count = 0;
+        for (int i = 0; i < faces; i++) {
+            if (countNow == maxCount){
+                count++;
+            }
+            else{
+                count += recursiveCount(countNow + 1, maxCount, faces);
+            }
+        }
+        // for (int i = 0; i < faces; i++) { // O(n^4)
+        //     for (int j = 0; j < faces; j++) {
+        //         for (int j2 = 0; j2 < faces; j2++) {
+        //             for (int k = 0; k < faces; k++) {
+        //                 count+=1;
+        //             }
+        //         }
+        //     }
+        // }
+        return count;
+    }
+
+    // РџРёС€РµРј Р°Р»РіРѕСЂРёС‚Рј РїРѕРёСЃРєР° РЅСѓР¶РЅРѕРіРѕ С‡РёСЃР»Р° РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚Рё Р¤РёР±РѕРЅР°С‡С‡Рё. 
+    // РЎС‡РёС‚Р°РµРј, С‡С‚Рѕ 1 Рё 2 Р·РЅР°С‡РµРЅРёСЏ РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚Рё СЂР°РІРЅС‹ 1
+    public static void task4(){
+        int n = 50;
+        Date startData = new Date(System.currentTimeMillis());
+        System.out.println(fb(n));
+        Date endData = new Date(System.currentTimeMillis());
+        Long chislo = endData.getTime() - startData.getTime();
+        System.out.println("Р’СЂРµРјСЏ РёСЃРїРѕР»РЅРµРЅРёСЏ: " + chislo);
+
+
+        Date startData2 = new Date(System.currentTimeMillis());
+        System.out.println(fb2(n));
+        Date endData2 = new Date(System.currentTimeMillis());
+        Long chislo2 = endData2.getTime() - startData2.getTime();
+        System.out.println("Р’СЂРµРјСЏ РёСЃРїРѕР»РЅРµРЅРёСЏ: " + chislo2);
+        // 1 1 2 3
+    }
+    public static int fb(int num){ //O(2^n)
+        //On = On-1 + On-2
+        if (num <= 2){
+            return 1;
+        }
+        else{
+            return fb(num-1) + fb(num -2);
+        }
+    }
+
+
+    public static int fb2(int num){ // O(n)
+        if (num <= 2){
+            return 1;
+        }
+        int [] numbers = new int[num];
+        numbers[0] = 1;
+        numbers[1] = 1;
+        for (int i = 2; i < numbers.length; i++) {
+            numbers[i] = numbers[i-1] + numbers[i-2];
+        }
+        return numbers[num-1];
+    }
+
+
+
+}
