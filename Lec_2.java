092c2619a5abca03adf6790c823c8a6b69350c7d
Julia Ch <juliecha@yandex.ru>
2023-06-15 12:59:17 +0300

    UPD Lec_2 - Array: search and sort

--- Lec_2.java
+++ Lec_2.java
@@ -40,10 +40,18 @@ public class Lec_2 {
     System.out.println("Искомый индекс: " + findIndex(findValue, array));
     System.out.println("Искомый индекс: " + binarySearch(findValue, array, 0, array.length-1));
 
-    int[] array4 = new int[] {35, 1, 22, 11,34,5,2,16,5,7,9,3};
+    int[] array4 = new int[] {35,1,22,11,34,5,2,16,5,7,9,3};
     quickSort(array4, 0, array4.length-1);
     for (int i = 0; i < array4.length; i++) {
       System.out.print(array4[i] + " ");
+    }
+
+    System.out.println();
+
+    int[] array5 = new int[] {35,1,5,2,16,5,7,9,3};
+    quickSort(array5, 0, array5.length-1);
+    for (int i = 0; i < array5.length; i++) {
+      System.out.print(array5[i] + " ");
     }
   }
   
@@ -178,6 +186,61 @@ public class Lec_2 {
     }
     if (startPos < rightPos) {
       quickSort(array, startPos, rightPos);
+    }
+  }
+
+
+  // Особенность данной сортировки в использовании дополнительной структуры
+  // данных называемой бинарной кучей (пирамидой).
+  // Бинарная куча представляет из себя древовидную структуру, когда у каждого
+  // объекта может быть до 2 детей. При этом строится из массива она предельно
+  // просто – первый элемент массива является корнем, 2 и 3 его детьми, 4 и 5 детьми
+  // элемента 2 и т.д. пока в массиве остаются элементы.
+  // если принять элемент с индексом i за родителя, то индексы его дочерних
+  // элементов будут 2 * i + 1 и 2 * i + 2
+
+  // Сортировка кучей (пирамидальная)
+  public static void heapSort(int[] array) {
+    // построение кучи (перегруппируем массив)
+    for (int i = array.length / 2 - 1; i >= 0; i--) {
+      heapify(array, array.length, i);
+    }
+
+    // один за другим извлекаем элементы из кучи
+    for (int i = array.length - 1; i > 0; i--) {
+      // премещаем текущий корень в конец
+      int temp = array[0];
+      array[0] = array[i];
+      array[i] = temp;
+
+      // вызываем процедуру heapify на уменьшенной куче
+      heapify(array, i, 0);
+    }
+  }
+
+  public static void heapify(int[] array, int heapSize, int rootIndex) {
+    int largest = rootIndex; // инициализируем самый большой элемент как корень
+    int leftChild = 2 * rootIndex + 1; // левый  2*rootIndex + 1
+    int rightChild = 2 * rootIndex + 2; // правый  2*rootIndex + 2
+
+    // если левый дочерний элемент больше корня
+    if (leftChild < heapSize && array[leftChild] > array[largest]) {
+      largest = leftChild;
+    }
+
+    // если правый дочерний элемент больше чем самый большой элемент на данный момент
+    if (rightChild < heapSize && array[rightChild] > array[largest]) {
+      largest = rightChild;
+    }
+
+    // если самый большой элемнт не корень
+    if (largest != rootIndex) {
+      int temp = array[rootIndex];
+      array[rootIndex] = array[largest];
+      array[largest] = temp;
+      
+      // рекурсивно преобразуем в двоичную кучу затронутое поддерево
+      heapify(array, heapSize, largest);
     }
   }
 }
