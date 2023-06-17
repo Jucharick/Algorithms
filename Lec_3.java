1172cb1a04f4bd5d846b4705e385cede7a3682e9
Julia Ch <juliecha@yandex.ru>
2023-06-17 16:39:50 +0300

    UPD Lec_3 - List

--- Lec_3.java
+++ Lec_3.java
@@ -13,20 +13,89 @@
 // последовательности (для двунаправленного) или просто добавить новый узел в
 // конец цепочки.
 
+// Преимущества связного списка
+// ● Массовые вставки и удаления в конец\начало
+// списка
+// ● Массовые вставки и удаления в середину
+// списка, если операция поиска выполняется
+// единожды
+// ● Динамическая расширяемость 
+
 public class Lec_3 {
   private Node head;
   private Node tail;
 
-public Node findNode(int value) { // сложность  O(n)
-  Node currentNode = head;
-  while (currentNode.nextNode != null) {
-    currentNode = currentNode.nextNode;
-    if (currentNode.value == value) {
-      return currentNode;
+  public Node findNode(int value) { // сложность  O(n)
+    Node currentNode = head;
+    while (currentNode.nextNode != null) {
+      currentNode = currentNode.nextNode;
+      if (currentNode.value == value) {
+        return currentNode;
+      }
+    }
+    return null;
+  }
+
+  public void addLast(int value) {
+    Node node = new Node();
+    node.value = value;
+    if (head == null) {
+      head = node;
+      tail = node;
+    } else {
+      tail.nextNode = node;
+      node.previousNode = tail;
+      tail = node;
     }
   }
-  return null;
-}
+
+  public void add(int value, Node node) {
+    Node next = node.nextNode;
+    Node newNode = new Node();
+    newNode.value = value;
+    node.nextNode = newNode;
+    newNode.previousNode = node;
+    if (next == null) {
+      tail = newNode;
+    } else {
+      next.previousNode = newNode;
+      newNode.nextNode = next;
+    }
+  }
+
+  public void delete(Node node) {
+    Node  previous = node.previousNode;
+    Node next = node.nextNode;
+    if (previous == null) {
+      next.previousNode = null;
+      head = next;
+    } else {
+      if (next == null) {
+        previous.nextNode = null;
+        tail = previous;
+      } else {
+        previous.nextNode = next;
+        next.previousNode = previous;
+      }
+    }
+  }
+
+  public void revert(){
+    Node currentNode = head;
+    while (currentNode != null) {
+      Node next = currentNode.nextNode;
+      Node  previous = currentNode.previousNode;
+      currentNode.nextNode = previous;
+      currentNode.previousNode = next;
+      if (previous == null) {
+        tail = currentNode;
+      }
+      if (next == null) {
+        head = currentNode;
+      }
+      currentNode = next;
+    }
+  }
 
   class Node { // Node - классическое название для подобных структур
     private int value;
