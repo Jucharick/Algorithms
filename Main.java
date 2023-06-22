36efa92dbb36db77cef7ec528dc9bcc54f647f1b
Julia Ch <juliecha@yandex.ru>
2023-06-22 19:22:55 +0300

    UPD Sem_3 - LinkedList and LinkedListT

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedList list= new LinkedList();
+    list.add(4);
+    list.add(5);
+    list.add(2);
+    list.add(1);
+    list.add(4);
+
+    list.print();
+
+    list.remove(2);
+
+    list.print();
+  }
+  
+}
